658378255dc1fc3b0a5dbf891a42d77ffec9109e
罗天旭 <1265062404@qq.com>
2021-08-07 18:11:37 +0800

    redis

---
+++ system-oauth2/src/main/java/com/system/admin/AuthServerApplication.java
@@ -0,0 +1,20 @@
+package com.system.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Luotianxu
+ */
+@EnableFeignClients // 扫描Feign接口
+@EnableDiscoveryClient // 标识nacos客户端
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+}
